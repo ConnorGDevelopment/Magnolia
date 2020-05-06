067c31997a90a276e4066303b7e489b9b6fbd85f
ConnorGDevelopment <connorgdevelopment@gmail.com>
2020-05-06 11:21:14 -0400

    Meta Data

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,15 +4,15 @@ export default {
      ** Headers of the page
      */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: 'The Magnolia Salon Boutique',
+    title: 'The Magnolia Salon Boutique',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'description', name: 'description', content: 'Dublin Ohio Hair Salon' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/img/favicon.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Cinzel&display=swap' },
       {
         rel: 'stylesheet',
